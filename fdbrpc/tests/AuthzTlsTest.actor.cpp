dbef00fa1d7e2e00906a17b6c22b04a4f7a92a50
Mark Shabanov <mshabanov@openintegration.inc>
2026-02-03 16:05:51 +0300

    XDB-478 fixed segfault with wrong client certificate password

--- fdbrpc/tests/AuthzTlsTest.actor.cpp
+++ fdbrpc/tests/AuthzTlsTest.actor.cpp
@@ -42,6 +42,21 @@
 
 std::FILE* outp = stdout;
 
+enum ExitCodes : int {
+	SUCCESS = 0,
+
+	MAIN_TEST_FAILED = 1,
+
+	CLIENT_PIPE_READ_ADDR_FAILED = 2,
+	CLIENT_FAILED = 3,
+	CLIENT_TEST_RESULT_MISMATCH = 4,
+
+	SERVER_BIND_ERROR = 5,
+	SERVER_STDOUT_REDIRECT_FAILED = 6,
+
+	WAITPID_ANY_STATUS = -1,
+};
+
 enum ChainLength : int {
 	NO_TLS = std::numeric_limits<int>::min(),
 };
@@ -51,6 +66,7 @@ void logRaw(const fmt::format_string<Args...>& fmt_str, Args&&... args) {
 	auto buf = fmt::memory_buffer{};
 	fmt::format_to(std::back_inserter(buf), fmt_str, std::forward<Args>(args)...);
 	fmt::print(outp, "{}", std::string_view(buf.data(), buf.size()));
+	fflush(outp);
 }
 
 template <class... Args>
@@ -58,6 +74,7 @@ void logWithPrefix(const char* prefix, const fmt::format_string<Args...>& fmt_st
 	auto buf = fmt::memory_buffer{};
 	fmt::format_to(std::back_inserter(buf), fmt_str, std::forward<Args>(args)...);
 	fmt::print(outp, "{}{}\n", prefix, std::string_view(buf.data(), buf.size()));
+	fflush(outp);
 }
 
 template <class... Args>
@@ -94,29 +111,38 @@ struct TLSCreds {
 	std::string certBytes;
 	std::string keyBytes;
 	std::string caBytes;
+	std::string password;
 };
 
-TLSCreds makeCreds(ChainLength chainLen, mkcert::ESide side) {
+TLSCreds makeCreds(ChainLength chainLen, mkcert::ESide side, StringRef password = {}) {
 	if (chainLen == 0 || chainLen == NO_TLS) {
-		return TLSCreds{ chainLen == NO_TLS, "", "", "" };
+		return TLSCreds{ chainLen == NO_TLS, "", "", "", "" };
 	}
 	auto arena = Arena();
 	auto ret = TLSCreds{};
-	auto specs = mkcert::makeCertChainSpec(arena, std::labs(chainLen), side);
-	if (chainLen < 0) {
-		specs[0].offsetNotBefore = -60l * 60 * 24 * 365;
-		specs[0].offsetNotAfter = -10l; // cert that expired 10 seconds ago
-	}
-	auto chain = mkcert::makeCertChain(arena, specs, {} /* create root CA cert from spec*/);
-	if (chain.size() == 1) {
-		ret.certBytes = concatCertChain(arena, chain).toString();
+	if (!password.empty()) {
+		ret.password = password.toString();
+		auto certAndKeyPem = mkcert::makePasswCert(arena, password);
+		ret.certBytes = certAndKeyPem.certPem.toString();
+		ret.keyBytes = certAndKeyPem.privateKeyPem.toString();
+		ret.caBytes = ret.certBytes;
 	} else {
-		auto nonRootChain = chain;
-		nonRootChain.pop_back();
-		ret.certBytes = concatCertChain(arena, nonRootChain).toString();
+		auto specs = mkcert::makeCertChainSpec(arena, std::labs(chainLen), side);
+		if (chainLen < 0) {
+			specs[0].offsetNotBefore = -60l * 60 * 24 * 365;
+			specs[0].offsetNotAfter = -10l; // cert that expired 10 seconds ago
+		}
+		auto chain = mkcert::makeCertChain(arena, specs, {} /* create root CA cert from spec*/);
+		if (chain.size() == 1) {
+			ret.certBytes = concatCertChain(arena, chain).toString();
+		} else {
+			auto nonRootChain = chain;
+			nonRootChain.pop_back();
+			ret.certBytes = concatCertChain(arena, nonRootChain).toString();
+		}
+		ret.caBytes = chain.back().certPem.toString();
+		ret.keyBytes = chain.front().privateKeyPem.toString();
 	}
-	ret.caBytes = chain.back().certPem.toString();
-	ret.keyBytes = chain.front().privateKeyPem.toString();
 	return ret;
 }
 
@@ -270,6 +296,7 @@ int runHost(TLSCreds creds, int addrPipe, int completionPipe, Result expect) {
 		tlsConfig.setCertificateBytes(creds.certBytes);
 		tlsConfig.setCABytes(creds.caBytes);
 		tlsConfig.setKeyBytes(creds.keyBytes);
+		tlsConfig.setPassword(creds.password);
 	}
 	g_network = newNet2(tlsConfig);
 	openTraceFile({}, 10 << 20, 10 << 20, ".", IsServer ? "authz_tls_unittest_server" : "authz_tls_unittest_client");
@@ -279,7 +306,12 @@ int runHost(TLSCreds creds, int addrPipe, int completionPipe, Result expect) {
 		auto addr = NetworkAddress::parse(noTls ? "127.0.0.1:0" : "127.0.0.1:0:tls");
 		auto endpoint = Endpoint();
 		auto receiver = SessionProbeReceiver();
-		auto listenFuture = transport.bind(addr, addr);
+		try {
+			transport.bind(addr, addr);
+		} catch (const Error& err) {
+			logs("CAUGHT Error in bind: code={} what={}", err.code(), err.what());
+			return SERVER_BIND_ERROR;
+		}
 		transport.addEndpoint(endpoint, &receiver, TaskPriority::ReadSocket);
 		auto thread = std::thread([]() {
 			g_network->run();
@@ -290,13 +322,13 @@ int runHost(TLSCreds creds, int addrPipe, int completionPipe, Result expect) {
 			g_network->stop();
 			thread.join();
 		});
-		return 0;
+		return SUCCESS;
 	} else {
 		auto dest = Endpoint();
 		auto& serverAddr = dest.addresses.address;
 		if (sizeof(serverAddr) != ::read(addrPipe, &serverAddr, sizeof(serverAddr))) {
 			logc("Failed to read server addr from pipe: {}", strerror(errno));
-			return 1;
+			return CLIENT_PIPE_READ_ADDR_FAILED;
 		}
 		if (noTls)
 			serverAddr.flags &= ~NetworkAddress::FLAG_TLS;
@@ -305,14 +337,14 @@ int runHost(TLSCreds creds, int addrPipe, int completionPipe, Result expect) {
 		auto& token = dest.token;
 		if (sizeof(token) != ::read(addrPipe, &token, sizeof(token))) {
 			logc("Failed to read server endpoint token from pipe: {}", strerror(errno));
-			return 2;
+			return CLIENT_FAILED;
 		}
 		logc("Server address is {}{}", serverAddr.toString(), noTls ? " (TLS suffix removed)" : "");
 		logc("Server endpoint token is {}", token.toString());
 		auto sessionProbeReq = SessionProbeRequest{};
 		transport.sendUnreliable(SerializeSource(sessionProbeReq), dest, true /*openConnection*/);
 		logc("Request is sent");
-		auto rc = 0;
+		auto rc = SUCCESS;
 		auto result = Result::ERROR;
 		{
 			auto timeout = delay(expect == Result::TIMEOUT ? 0.5 : 5);
@@ -323,12 +355,12 @@ int runHost(TLSCreds creds, int addrPipe, int completionPipe, Result expect) {
 		auto done = true;
 		if (sizeof(done) != ::write(completionPipe, &done, sizeof(done))) {
 			logc("Failed to signal server to terminate: {}", strerror(errno));
-			rc = 4;
+			rc = CLIENT_FAILED;
 		}
-		if (rc == 0) {
+		if (rc == SUCCESS) {
 			if (expect != result) {
 				logc("Test failed: expected {}, got {}", expect, result);
-				rc = 5;
+				rc = CLIENT_TEST_RESULT_MISMATCH;
 			} else {
 				logc("Response OK: got {} as expected", result);
 			}
@@ -355,46 +387,100 @@ Result getExpectedResult(ChainLength serverChainLen, ChainLength clientChainLen)
 	return expect;
 }
 
-bool waitPid(pid_t subProcPid, const char* procName) {
+std::pair<bool, std::string> waitPidStatusInterpreter(const char* procName, const int status) {
+	std::string prefix = fmt::format("{} subprocess ", procName);
+	std::string message;
+	if (WIFEXITED(status)) {
+		const auto exitStatus = WEXITSTATUS(status);
+		if (exitStatus == 0) {
+			return { true, fmt::format("{} waitpid() OK", prefix) };
+		}
+		message = fmt::format("{} exited with status {}", prefix, exitStatus);
+	} else if (WIFSIGNALED(status)) {
+		const auto signal = WTERMSIG(status);
+		message = fmt::format("{} killed by signal {} - {}", prefix, signal, strsignal(signal));
+#ifdef WCOREDUMP
+		const auto coreDumped = WCOREDUMP(status);
+		if (coreDumped)
+			message.append(std::string_view(" (core dumped)"));
+#endif // WCOREDUMP
+	} else if (WIFSTOPPED(status)) {
+		const auto signal = WSTOPSIG(status);
+		message = fmt::format("{} stopped by signal {} - {}", prefix, signal, strsignal(signal));
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wparentheses"
+	} else if (WIFCONTINUED(status)) {
+#pragma clang diagnostic pop
+		message = fmt::format("{} continued by signal SIGCONT", prefix);
+	}
+
+	if (message.empty()) {
+		message = fmt::format("{} Unrecognized status {} (Check man 2 waitpid for more details)", prefix, status);
+	}
+
+	return { false, message };
+}
+
+bool waitPid(pid_t subProcPid, const char* procName, int expectStatus = WAITPID_ANY_STATUS) {
 	auto status = int{};
 	auto pid = ::waitpid(subProcPid, &status, 0);
 	if (pid < 0) {
 		logm("{} subprocess waitpid() failed with {}", procName, strerror(errno));
 		return false;
 	} else {
-		if (status != 0) {
-			logm("{} subprocess had error: rc={}", procName, status);
-			return false;
-		} else {
-			logm("{} subprocess waitpid() OK", procName);
-			return true;
-		}
+		auto [ok, message] = waitPidStatusInterpreter(procName, status);
+		logm("{}", message);
+		return ok || (expectStatus != WAITPID_ANY_STATUS && WEXITSTATUS(status) == expectStatus);
 	}
 }
 
-int runTlsTest(ChainLength serverChainLen, ChainLength clientChainLen) {
-	logm("==== BEGIN TESTCASE ====");
-	auto const expect = getExpectedResult(serverChainLen, clientChainLen);
+int runTlsTest(ChainLength serverChainLen, ChainLength clientChainLen, std::string_view passwordTestCase = "") {
 	using namespace std::literals::string_literals;
-	logm("Cert chain length: server={} client={}", serverChainLen, clientChainLen);
-	auto arena = Arena();
-	auto serverCreds = makeCreds(serverChainLen, mkcert::ESide::Server);
-	auto clientCreds = makeCreds(clientChainLen, mkcert::ESide::Client);
-	// make server and client trust each other
-	std::swap(serverCreds.caBytes, clientCreds.caBytes);
+	auto expect = Result::TRUSTED;
+	TLSCreds serverCreds;
+	TLSCreds clientCreds;
+	int expectStatusServer = WAITPID_ANY_STATUS;
+	int expectStatusClient = WAITPID_ANY_STATUS;
+
+	if (passwordTestCase.empty()) {
+		logm("==== BEGIN TESTCASE ====");
+		expect = getExpectedResult(serverChainLen, clientChainLen);
+		logm("Cert chain length: server={} client={}", serverChainLen, clientChainLen);
+		serverCreds = makeCreds(serverChainLen, mkcert::ESide::Server);
+		clientCreds = makeCreds(clientChainLen, mkcert::ESide::Client);
+		// make server and client trust each other
+		std::swap(serverCreds.caBytes, clientCreds.caBytes);
+	} else {
+		const auto password = "abc123"_sr;
+		serverCreds = makeCreds(serverChainLen, mkcert::ESide::Server, password);
+		clientCreds = serverCreds;
+
+		if (passwordTestCase == "client") {
+			logm("==== BEGIN CLIENT BAD PASSWORD TESTCASE ====");
+			expect = Result::TIMEOUT;
+			clientCreds.password = "bad";
+		} else if (passwordTestCase == "server") {
+			logm("==== BEGIN SERVER BAD PASSWORD TESTCASE ====");
+			serverCreds.password = "bad";
+			expectStatusServer = SERVER_BIND_ERROR;
+			expectStatusClient = CLIENT_PIPE_READ_ADDR_FAILED;
+		} else {
+			logm("==== BEGIN PASSWORD PROTECTED TESTCASE ====");
+		}
+	}
 	auto clientPid = pid_t{};
 	auto serverPid = pid_t{};
 	int addrPipe[2], completionPipe[2], serverStdoutPipe[2], clientStdoutPipe[2];
 	if (::pipe(addrPipe) || ::pipe(completionPipe) || ::pipe(serverStdoutPipe) || ::pipe(clientStdoutPipe)) {
 		logm("Pipe open failed: {}", strerror(errno));
-		return 1;
+		return MAIN_TEST_FAILED;
 	}
 	auto ok = true;
 	{
 		serverPid = fork();
 		if (serverPid == -1) {
 			logm("fork() for server subprocess failed: {}", strerror(errno));
-			return 1;
+			return MAIN_TEST_FAILED;
 		} else if (serverPid == 0) {
 			// server subprocess
 			::close(addrPipe[0]); // close address-in pipe (server writes its own address for client)
@@ -410,23 +496,24 @@ int runTlsTest(ChainLength serverChainLen, ChainLength clientChainLen) {
 			if (-1 == ::dup2(serverStdoutPipe[1], STDOUT_FILENO)) {
 				logs("Failed to redirect server stdout to pipe: {}", strerror(errno));
 				::close(serverStdoutPipe[1]);
-				return 1;
+				return SERVER_STDOUT_REDIRECT_FAILED;
 			}
 			_exit(runHost<true>(std::move(serverCreds), addrPipe[1], completionPipe[0], expect));
 		}
-		auto serverProcCleanup = ScopeExit([&ok, serverPid]() {
-			if (!waitPid(serverPid, "Server"))
+		auto serverProcCleanup = ScopeExit([&ok, serverPid, expectStatusServer]() {
+			if (!waitPid(serverPid, "Server", expectStatusServer))
 				ok = false;
 		});
+		::close(addrPipe[1]);
+		::close(completionPipe[0]);
+		::close(serverStdoutPipe[1]);
+
 		clientPid = fork();
 		if (clientPid == -1) {
 			logm("fork() for client subprocess failed: {}", strerror(errno));
-			return 1;
+			return MAIN_TEST_FAILED;
 		} else if (clientPid == 0) {
-			::close(addrPipe[1]);
-			::close(completionPipe[0]);
 			::close(serverStdoutPipe[0]);
-			::close(serverStdoutPipe[1]);
 			::close(clientStdoutPipe[0]);
 			auto pipeCleanup = ScopeExit([&addrPipe, &completionPipe]() {
 				::close(addrPipe[0]);
@@ -435,21 +522,18 @@ int runTlsTest(ChainLength serverChainLen, ChainLength clientChainLen) {
 			if (-1 == ::dup2(clientStdoutPipe[1], STDOUT_FILENO)) {
 				logs("Failed to redirect client stdout to pipe: {}", strerror(errno));
 				::close(clientStdoutPipe[1]);
-				return 1;
+				return CLIENT_FAILED;
 			}
 			_exit(runHost<false>(std::move(clientCreds), addrPipe[0], completionPipe[1], expect));
 		}
-		auto clientProcCleanup = ScopeExit([&ok, clientPid]() {
-			if (!waitPid(clientPid, "Client"))
+		auto clientProcCleanup = ScopeExit([&ok, clientPid, expectStatusClient]() {
+			if (!waitPid(clientPid, "Client", expectStatusClient))
 				ok = false;
 		});
 	}
 	// main process
 	::close(addrPipe[0]);
-	::close(addrPipe[1]);
-	::close(completionPipe[0]);
 	::close(completionPipe[1]);
-	::close(serverStdoutPipe[1]);
 	::close(clientStdoutPipe[1]);
 	auto pipeCleanup = ScopeExit([&]() {
 		::close(serverStdoutPipe[0]);
@@ -464,7 +548,7 @@ int runTlsTest(ChainLength serverChainLen, ChainLength clientChainLen) {
 	logRaw(fmt::runtime(serverStdout));
 	logm("/// End Server STDOUT ///");
 	logm(fmt::runtime(ok ? "OK" : "FAILED"));
-	return !ok;
+	return ok ? SUCCESS : MAIN_TEST_FAILED;
 }
 
 int main(int argc, char** argv) {
@@ -494,17 +578,34 @@ int main(int argc, char** argv) {
 		if (runTlsTest(serverChainLen, clientChainLen))
 			failed.push_back({ serverChainLen, clientChainLen });
 	}
+
+	constexpr auto singleChainPair = std::pair(ChainLength(1), ChainLength(1));
+	inputs.insert(inputs.end(), 3, singleChainPair);
+
+	std::vector<std::string_view> failedPasswordTests;
+	for (const auto& testCase : std::array{ "no_bad_password", "client", "server" }) {
+		if (runTlsTest(singleChainPair.first, singleChainPair.second, testCase)) {
+			failed.push_back(singleChainPair);
+			failedPasswordTests.push_back(testCase);
+		}
+	}
+
 	if (!failed.empty()) {
+		if (!failedPasswordTests.empty()) {
+			for (const auto& test : failedPasswordTests) {
+				logm(" {}, failed", test);
+			}
+		}
 		logm("Test Failed: {}/{} cases: {}", failed.size(), inputs.size(), failed);
-		return 1;
+		return MAIN_TEST_FAILED;
 	} else {
 		logm("Test OK: {}/{} cases passed", inputs.size(), inputs.size());
-		return 0;
+		return SUCCESS;
 	}
 }
 #else // _WIN32
 
 int main() {
-	return 0;
+	return SUCCESS;
 }
 #endif // _WIN32
