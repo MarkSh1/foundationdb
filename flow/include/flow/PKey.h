c8f49cd9c4d75948471cddba5935d6d6c1545871
Mark Shabanov <mshabanov@openintegration.inc>
2026-02-12 13:23:56 +0300

    XDB-478 Fix segmentation fault, properly initialize activeTlsPolicy on password errors

--- flow/include/flow/PKey.h
+++ flow/include/flow/PKey.h
@@ -92,7 +92,10 @@ public:
 	PrivateKey& operator=(const PrivateKey& other) noexcept = default;
 
 	// PEM_write_bio_PrivateKey
-	StringRef writePem(Arena& arena) const;
+	StringRef writePem(Arena& arena, StringRef password = StringRef()) const;
+
+	// PEM_write_bio_PrivateKey with password encryption
+	StringRef writePemWithPassword(Arena& arena, StringRef password) const;
 
 	// d2i_PrivateKey
 	StringRef writeDer(Arena& arena) const;
