735dd99cef3e2ec9735f959e88b7a70943cb3932
Mark Shabanov <mshabanov@openintegration.inc>
2026-03-10 23:21:54 +0300

    XDB-494 added manual test run

--- build-scripts/for-linux/test-joshua.bash
+++ build-scripts/for-linux/test-joshua.bash
@@ -3,11 +3,9 @@
 # $1 - the path to the correctness archive file, ex. correctness-7.3.49-2.ow.tar.gz
 # $2 - number of tests
 
-res=$(python3 -m joshua.joshua start --tarball $1 --max-runs $2 && python3 -m joshua.joshua tail --errors)
+python3 -m joshua.joshua start --tarball $1 --max-runs $2 && python3 -m joshua.joshua tail | tee /tmp/joshua_output.txt
 
-substr="TestUID"
-echo $res
-if [[ $res == *$substr* ]]; then
+if grep -q 'Ok="0"' /tmp/joshua_output.txt; then
   echo >&2 "Test failed."
   exit 1
 fi
